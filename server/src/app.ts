8697f8e0db1ac81def784b3e3a45341f80597f58
harshhirpara2612 <18ce030@charusat.edu.in>
2026-04-13 20:52:06 -0400

    Cors Updated

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://smart-job-tracker-git-main-harsh0826s-projects.vercel.app/",
+  "https://smart-job-tracker-git-main-harsh0826s-projects.vercel.app",
 ];
 
 app.use(
